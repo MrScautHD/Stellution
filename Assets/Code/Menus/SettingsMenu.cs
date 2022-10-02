019ef15f102edad6af69386409c48abf0db847fd
MrScautHD <MrScautHD5@gmail.com>
2022-10-02 14:07:13 +0200

    Add Settings Menu

---
+++ Assets/Code/Menus/SettingsMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionsDropdown;
+    
+    private Resolution[] resolutions;
+    
+    public void Start()
+    {
+        this.resolutions = Screen.resolutions;
+        
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+        
+        resolutionsDropdown.AddOptions(options);
+    }
+
+    public void SetResolution(int resolutionsIndex)
+    {
+        Resolution resolution = this.resolutions[resolutionsIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    
+    public void SetVolume(float volume)
+    {
+        this.audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
