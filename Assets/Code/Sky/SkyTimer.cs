8cb9a57b070f830d54d8051397beb11965ef45e8
MrScautHD <MrScautHD5@gmail.com>
2022-10-08 08:58:21 +0200

    Set Default Time to 6 am

--- Assets/Code/Sky/SkyTimer.cs
+++ Assets/Code/Sky/SkyTimer.cs
@@ -5,18 +5,18 @@ public class SkyTimer : MonoBehaviour
 {
 
     public AtmosPad atmosPad;
-    private float timer;
+    public float time;
 
     public void Update()
     {
         float deltaTime = Time.deltaTime;
-        this.timer += deltaTime / 30;
+        this.time += deltaTime / 30;
 
         if (this.atmosPad.Hour > 24)
         {
-            this.timer = 0;
+            this.time = 0;
         }
         
-        this.atmosPad.SetHour(this.timer);
+        this.atmosPad.SetHour(this.time);
     }
 }
