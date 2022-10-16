5c28269f2d6f6d84e060cc7f20c61ce4de337b0d
MrScautHD <MrScautHD5@gmail.com>
2022-10-16 20:49:40 +0200

    improved SkyTimer

--- Assets/Script/Sky/SkyTimer.cs
+++ Assets/Script/Sky/SkyTimer.cs
@@ -9,8 +9,12 @@ public class SkyTimer : NetworkBehaviour {
     private NetworkVariable<float> timeOfDay = new NetworkVariable<float>(6, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     
     public void Update() {
-        this.timeOfDay.Value += 0.001F;
-        this.timeOfDay.Value %= 24;
+
+        this.timeOfDay.Value += 8e-5F;
+
+        if (this.atmosPad.Hour > 24) {
+            this.timeOfDay.Value = 0;
+        }
 
         this.atmosPad.SetHour(this.timeOfDay.Value);
     }
