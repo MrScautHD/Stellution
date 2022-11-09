4b97157661d3b3e9401f6c9fe2e09199bdc65457
MrScautHD <MrScautHD5@gmail.com>
2022-11-09 23:02:57 +0100

    Start on Car physic

--- Assets/Script/Common/Entity/Vehicle/FlyingCar.cs
+++ Assets/Script/Common/Entity/Vehicle/FlyingCar.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class FlyingCar : AbstractVehicle {
 
     [SerializeField] private Rigidbody carBody;
-
+    
+    [SerializeField] private Transform[] hoverPoints;
+    [SerializeField] private float hoverForce = 65;
+    [SerializeField] private float hoverHeight = 6.5F;
+    
     [SerializeField] private float speed = 20;
 
     private void Update() {
@@ -13,16 +17,29 @@ public class FlyingCar : AbstractVehicle {
         this.PassangerMovement();
     }
 
+    public void FixedUpdate() {
+        base.FixedUpdate();
+        
+        this.HoverForce();
+    }
+
     public override float SeatHeight() {
         return 1.6F;
     }
 
     private void Move() {
-        if (this.GetMainPassenger() != null) {
-            float horizontalInput = Input.GetAxis("Vertical");
-            
-            Vector3 move = this.transform.right * horizontalInput;
-            this.carBody.MovePosition(move * this.speed * Time.deltaTime);
+
+    }
+
+    private void HoverForce() {
+        foreach (Transform hoverPoint in this.hoverPoints) {
+            Ray ray = new Ray(hoverPoint.position, -hoverPoint.up);
+
+            if (Physics.Raycast(ray, out var hit, this.hoverHeight)) {
+                float proportionalHeight = (this.hoverHeight = hit.distance) / this.hoverHeight;
+                Vector3 appliedHoverForce = Vector3.up * proportionalHeight * this.hoverForce;
+                carBody.AddForceAtPosition(appliedHoverForce, hoverPoint.position, ForceMode.Acceleration);
+            }
         }
     }
 
