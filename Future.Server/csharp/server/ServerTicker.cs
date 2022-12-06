bd5f3de71a587ffb1d7921a4e112186a072c0768
MrScautHD <MrScautHD5@gmail.com>
2022-12-06 09:55:07 +0100

    Update to c# 11 and Net. 7

--- Future.Server/csharp/server/ServerTicker.cs
+++ Future.Server/csharp/server/ServerTicker.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 
 namespace Future.Server.csharp.server;
@@ -5,19 +6,27 @@ namespace Future.Server.csharp.server;
 public class ServerTicker {
     
     private GameTime _gameTime;
-    private TimeSpan _gameTimer;
+    
+    private int _updateFrameLag;
+
+    private long _previousTicks;
+    
+    private Stopwatch _gameTimer;
+    
+    private TimeSpan _accumulatedElapsedTime;
+    private TimeSpan _maxElapsedTime = TimeSpan.FromMilliseconds(500.0);
+    public TimeSpan TargetElapsedTime;
 
     private double _timer;
     private readonly double _delay = 1.0 / 60.0;
 
     public ServerTicker() {
         this._gameTime = new GameTime();
-        this._gameTimer = this._gameTime.TotalGameTime;
     }
 
     protected void RunTick(bool isRuning) {
-        while (isRuning) {
-            this._gameTimer += this._gameTime.ElapsedGameTime;
+        /*while (isRuning) {
+            //this._gameTimer += this._gameTime.ElapsedGameTime;
 
             // UPDATE
             this.Update(this._gameTime);
@@ -29,16 +38,26 @@ public class ServerTicker {
                 this.FixedUpdate(this._gameTime);
                 this._timer -= this._delay;
             }
+        }*/
+        
+        this.SetupGameTime(isRuning);
+    }
+
+    private void SetupGameTime(bool isRuning) {
+        while (isRuning) {
+            this._gameTime.ElapsedGameTime = _gameTime.ElapsedGameTime;
         }
+
+        //this._gameTime.ElapsedGameTime = TimeSpan.FromTicks(this.TargetElapsedTime.Ticks * (long) num);
     }
 
     // EVERY FRAME!
     protected virtual void Update(GameTime gameTime) {
-        
+        Console.WriteLine(gameTime);
     }
 
     // EVERY 60 TICKS PER SEC!
     protected virtual void FixedUpdate(GameTime gameTime) {
-        Console.WriteLine(gameTime);
+        
     }
 }
