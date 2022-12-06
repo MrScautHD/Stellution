2f63304869e3fb80a32170358e7781d91ab91272
MrScautHD <MrScautHD5@gmail.com>
2022-12-06 11:41:07 +0100

    Update ServerTicker.cs

--- Future.Server/csharp/server/ServerTicker.cs
+++ Future.Server/csharp/server/ServerTicker.cs
@@ -13,56 +13,56 @@ public class ServerTicker {
     
     private TimeSpan _accumulatedElapsedTime;
     
-    private TimeSpan _targetElapsedTime = TimeSpan.FromTicks(166667L);
-    private TimeSpan _maxElapsedTime = TimeSpan.FromMilliseconds(500.0);
-    private TimeSpan _inactiveSleepTime = TimeSpan.FromSeconds(0.02);
+    private readonly TimeSpan _targetElapsedTime = TimeSpan.FromTicks(166667L);
+    private readonly TimeSpan _maxElapsedTime = TimeSpan.FromMilliseconds(500.0);
+    private readonly TimeSpan _inactiveSleepTime = TimeSpan.FromSeconds(0.02);
     
     private double _timer;
     private readonly double _delay = 1.0 / 60.0;
 
-    protected void RunTick(bool isRuning) { 
+    protected void RunTick(bool isRunning) {
         while (true) {
             do {
                 if (this._inactiveSleepTime.TotalMilliseconds >= 1.0) {
-                    Thread.Sleep((int) this._inactiveSleepTime.TotalMilliseconds);
+                    Thread.Sleep((int)this._inactiveSleepTime.TotalMilliseconds);
                 }
-            
+
                 if (this._gameTimer == null) {
                     this._gameTimer = new Stopwatch();
-                    this._gameTimer.Start(); 
+                    this._gameTimer.Start();
                 }
-          
+
                 long ticks = this._gameTimer.Elapsed.Ticks;
                 this._accumulatedElapsedTime += TimeSpan.FromTicks(ticks - this._previousTicks);
                 this._previousTicks = ticks;
 
                 if (!(this._accumulatedElapsedTime < this._targetElapsedTime)) {
                     if (this._accumulatedElapsedTime > this._maxElapsedTime) {
-                       this._accumulatedElapsedTime = this._maxElapsedTime;
+                        this._accumulatedElapsedTime = this._maxElapsedTime;
                     }
 
                     this._gameTime.ElapsedGameTime = this._targetElapsedTime;
-      
+
                     int num = 0;
-                    while (this._accumulatedElapsedTime >= this._targetElapsedTime && isRuning) {
+                    while (this._accumulatedElapsedTime >= this._targetElapsedTime && isRunning) {
                         this._gameTime.TotalGameTime += this._targetElapsedTime;
                         this._accumulatedElapsedTime -= this._targetElapsedTime;
                         ++num;
-          
+
                         // UPDATE
                         this.Update(this._gameTime);
-          
+
                         // FIXED UPDATE
                         this._timer += this._gameTime.ElapsedGameTime.TotalSeconds;
-        
+
                         if (this._timer >= this._delay) {
                             this.FixedUpdate(this._gameTime);
                             this._timer -= this._delay;
                         }
                     }
-      
+
                     this._updateFrameLag += Math.Max(0, num - 1);
-      
+
                     if (this._gameTime.IsRunningSlowly) {
                         if (this._updateFrameLag == 0) {
                             this._gameTime.IsRunningSlowly = false;
@@ -76,10 +76,10 @@ public class ServerTicker {
                         --this._updateFrameLag;
                     }
 
-                    this._gameTime.ElapsedGameTime = TimeSpan.FromTicks(this._targetElapsedTime.Ticks * (long) num);
+                    this._gameTime.ElapsedGameTime = TimeSpan.FromTicks(this._targetElapsedTime.Ticks * (long)num);
                 }
             }
-        
+            
             while ((this._targetElapsedTime - this._accumulatedElapsedTime).TotalMilliseconds < 2.0);
             Thread.Sleep(1);
         }
