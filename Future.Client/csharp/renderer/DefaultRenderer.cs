4b288fa8decb00411763449d318f1f5d8ec3a47f
MrScautHD <MrScautHD5@gmail.com>
2023-01-02 11:29:44 +0100

    Update DefaultRenderer.cs

--- Future.Client/csharp/renderer/DefaultRenderer.cs
+++ Future.Client/csharp/renderer/DefaultRenderer.cs
@@ -4,7 +4,6 @@ using Liru3D.Models;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.Extended;
 
 namespace Future.Client.csharp.renderer;
 
