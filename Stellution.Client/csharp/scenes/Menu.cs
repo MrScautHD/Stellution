660e93026750696f2eebf55af8f95092648bbb68
MrScautHD <MrScautHD5@gmail.com>
2023-04-09 23:12:39 +0200

    small clean up of Menu

--- Stellution.Client/csharp/scenes/Menu.cs
+++ Stellution.Client/csharp/scenes/Menu.cs
@@ -2,6 +2,7 @@ using Easel.Graphics;
 using Easel.GUI;
 using Easel.Math;
 using Stellution.Client.csharp.registry;
+using Stellution.Client.csharp.translation;
 using Stellution.Client.csharp.ui.elements;
 using Stellution.Common.csharp.scenes;
 
@@ -18,14 +19,8 @@ public class Menu : ModifiedScene {
 
         UI.DefaultStyle.Font = FontRegistry.Fontoe;
 
-        //UI.Add(new Panel("test", new Position(Anchor.CenterCenter), new Size<int>(90)));
-        //UI.Add(new ImageElement("image", Texture2D.White, new Position(Anchor.CenterCenter), new Size<int>(500)));
-        UI.Add(new ButtonElement("button", Texture2D.Missing, "BUTTON", 80, new Position(Anchor.CenterCenter), new Size<int>(400), true, null, Color.Aqua));
-        //UI.Add(new LabelElement("label", "IT WORKS!", new Position(Anchor.CenterCenter), 40, true, Color.Aqua));
-
-        //UI.Add("logo", new Image(new Position(Anchor.TopRight), ClientTextureRegistry.Get(ClientTextureRegistry.LogoTexture), new Size<int>(250)));
-        //UI.Add("label", new Label(new Position(Anchor.CenterCenter), "label", 20));
-
-        //UI.Add("button", new Button(new Position(Anchor.CenterCenter), new Size<int>(200, 40), Translation.Lang.Get("gui.button.singleplayer")));
+        UI.Add(new ImageElement("image", TextureRegistry.Female, new Position(Anchor.CenterCenter), new Size<int>(500)));
+        UI.Add(new ButtonElement("button", Texture2D.Missing, "BUTTON", 80, new Position(Anchor.CenterLeft), new Size<int>(400), true, null, Color.Aqua));
+        UI.Add(new LabelElement("label", Translation.Lang.Get("gui.button.singleplayer"), new Position(Anchor.CenterRight), 40, true, Color.Aqua));
     }
 }
